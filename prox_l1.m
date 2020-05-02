0f1ab3446d51ba73cb7f5b9f94ed85cb7ea96b32
Jorge Sepulveda <jsepulveda@dim.uchile.cl>
2020-05-02 15:57:18 -0400

    "minor change"

--- prox_l1.m
+++ prox_l1.m
@@ -1,6 +1,6 @@
 function [ prox_x ] = prox_l1( x, lambda )
-% X = prox_l1(x,lambda) 
 % Soft-thresholding/shrinkage operator
+% X = prox_l1(x,lambda) 
 
 
 prox_x =  subplus(abs(x) - lambda) .* sign(x);
